5fbf319a236ff2ddfad643a41de487e7883f65b7
suvi46 <suwarnawandhekar46@gmail.com>
2025-01-04 17:55:12 +0530

    switch case and rearranged in folder

---
+++ practise/switchcase.js
@@ -0,0 +1,52 @@
+function checkDayOfWeek(dayNumber) {
+  let day;
+  switch (dayNumber) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Invalid");
+      break;
+  }
+  return day;
+}
+console.log(checkDayOfWeek(5));
+console.log(checkDayOfWeek(0));
+console.log(checkDayOfWeek(1));
+console.log(checkDayOfWeek(30));
+
+// another example
+
+let browser = "chrome";
+switch (browser) {
+  case "chrome":
+    console.log("launch chrome");
+    break;
+  case "firefox":
+    console.log("launch firefox");
+    break;
+  case "edge":
+    console.log("launch edge");
+    break;
+  default:
+    console.log("please pass the right browser");
+    break;
+}
