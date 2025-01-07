69a3c2a735bb1e304e6036eeeba58c94c846b428
suvi46 <suwarnawandhekar46@gmail.com>
2025-01-07 10:09:10 +0530

    find typeof Operator

---
+++ practise/typeOfOperator.js
@@ -0,0 +1,34 @@
+const x = "suvi";
+console.log(typeof x);
+const y = 1;
+console.log(typeof y);
+
+let m1 = [2, 3, 4, 5, 6, 7];
+console.log(typeof m1);
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(typeof add);
+
+let employee = [
+  {
+    name: "a",
+    age: "25",
+    gender: "M",
+  },
+  {
+    name: "b",
+    age: "26",
+    gender: "F",
+  },
+];
+
+console.log(typeof employee);
+let z = 1;
+z = "s";
+console.log(typeof z); //string
+
+var a = 3;
+console.log(typeof a);
