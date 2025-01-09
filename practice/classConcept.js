06a21bc36e8636d3cda76ee6c8e0e5ab14e8a7a9
suvi46 <suwarnawandhekar46@gmail.com>
2025-01-10 00:04:04 +0530

    method in class

--- practice/classConcept.js
+++ practice/classConcept.js
@@ -5,10 +5,14 @@ class car {
     this.price = price;
   }
   refuel() {
-    console.log("car is refueled");
+    console.log(this.name + "car is refueled");
   }
 }
 const c1 = new car("BMW", "C1029", 2000); //new object with object reference varible c1
 console.log(c1.price + ": : " + c1.model + ": ");
 console.log(c1);
+c1.refuel();
 const c2 = new car("audi", "d29", 4000); //new object with object reference varible c2
+c2.refuel();
+
+// note: class may have only one constructor.can not overload constructor
